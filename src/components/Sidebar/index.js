cd292fb4b86946f9e65d624b530cc9de090828fc
joaoabo <82650085+joaoabo@users.noreply.github.com>
2022-06-02 22:16:16 -0300

    Criado os componentes MenuBar e PostItem, MenuBar vai ser um menu do site... PostItem onde vai ficar minha informações e meus trabalhos futurosjunto com cursos

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Profile from "../Profile"
 import SocialLinks from "../SocialLinks"
-import MenuLinks from '../MenuLinks/index'
+import MenuLinks from '../MenuLinks'
 
 import * as S from "./styled";
 
