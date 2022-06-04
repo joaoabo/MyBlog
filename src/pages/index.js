84c2cc4bf2167579c17a8288ef88e00463e60601
joaoabo <82650085+joaoabo@users.noreply.github.com>
2022-06-03 22:05:01 -0300

    Criado dois arquivos para trabalhar com markdonw, instalado plugin gatsby-transformer-remark para iniciar os estudos com graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,53 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import PostItem from "../components/PostItem"
 
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const { allMarkdonwRemark } = useStatticQuery(graphql`
+    query MyQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `)
+
+  const postList = allMarkdonwRemark.edges
+
+  return (
+    <Layout>
     <Seo title="Home" />
-    <PostItem
-      slug="/about/"
-      category="Misc"
-      date="30 de Julho de 2019"
-      timeToRead="5"
-      title="Diga não ao Medium: tenha sua própria plataforma"
-      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-    />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    {postList.map({
+      node: {
+        frontmatter: { background, category, date, description, title },
+        timeToRead
+      },
+    })}
+      <PostItem
+        slug="/about/"
+        category="Misc"
+        date="30 de Julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+      />
   </Layout>
-)
+  )
+}
+ 
 
 export default IndexPage
