f8d7a60ed30b0c1263ad618cc38e3ba5071da361
joaoabo <82650085+joaoabo@users.noreply.github.com>
2022-06-16 14:57:27 -0300

    Criada a pagina search, usando o algolia como motor de busca... usado os plugis do  gatsby gatsby-plugin-algolia-search react-instantsearch-ddon algoliasearch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config()
+
+const queries = require("./src/utils/algolia_queries")
+
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
@@ -61,6 +65,17 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
